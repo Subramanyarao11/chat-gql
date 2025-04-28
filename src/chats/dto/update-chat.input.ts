ac3c370b4782aaaffe770184fc339f099c57bf6f
SubramanyaRao11 <subramanya11rao@gmail.com>
2025-04-28 20:45:33 +0530

    Added: Chats endpoint

---
+++ src/chats/dto/update-chat.input.ts
@@ -0,0 +1,6 @@
+import { CreateChatInput } from './create-chat.input';
+import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+@InputType()
+export class UpdateChatInput extends PartialType(CreateChatInput) {
+  @Field(() => Int) id: number;
+}
